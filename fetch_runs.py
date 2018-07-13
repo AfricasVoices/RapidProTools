e258b397817230c5c17144d7e6297977df978d0c
Alexander Simpson <as2388@cam.ac.uk>
2018-07-13 13:58:14 +0100

    Update fetch runs to export runs as either messages or as a survey

--- fetch_runs.py
+++ fetch_runs.py
@@ -14,6 +14,12 @@ if __name__ == "__main__":
                         nargs="?", default=None)
     parser.add_argument("token", help="RapidPro API Token", nargs=1)
     parser.add_argument("user", help="User launching this program", nargs=1)
+    parser.add_argument("mode", help="How to interpret downloaded runs, either 'messages' or 'survey'. "
+                                     "If 'messages', treats each run as a radio show answer, and outputs multiple "
+                                     "answers per contact if multiple messages are received. "
+                                     "If 'survey', takes the latest value for each response field "
+                                     "(while maintaining the history of older values in TracedData)",
+                        nargs=1, choices=["messages", "survey"])
     parser.add_argument("output", help="Path to output file", nargs=1)
 
     args = parser.parse_args()
@@ -21,6 +27,7 @@ if __name__ == "__main__":
     flow_name = args.flow_name
     token = args.token[0]
     user = args.user[0]
+    mode = args.mode[0]
     output_path = args.output[0]
 
     rapid_pro = TembaClient(server, token)
@@ -42,7 +49,7 @@ if __name__ == "__main__":
 
         flow_id = matching_flows[0].uuid
 
-    # Download all flow runs which have been updated since the last poll.
+    # Download all runs for the requested flow.
     runs = rapid_pro.get_runs(flow=flow_id).all()
     # IMPORTANT: The .all() approach may not scale to flows with some as yet unquantified "large" number of runs.
     # See http://rapidpro-python.readthedocs.io/en/latest/#fetching-objects for more details.
@@ -51,35 +58,51 @@ if __name__ == "__main__":
     print("Fetched. Time taken: " + str(end - start))
 
     # TODO: Check if the next two steps match current AVF practice.
-    # TODO: Check if we should de-duplicate responses from participants who answer the survey more than once.
     # Ignore flows which are incomplete because the respondent is still working through the questions.
     runs = filter(lambda run: run.exited_on is not None, runs)
     # Ignore flows which are incomplete because the respondent stopped answering.
     runs = filter(lambda run: run.exit_type == "completed", runs)
 
     # Sort by ascending order of modification date
-    runs = list(runs)
     runs.reverse()
 
     print(str(len(runs)) + " runs will be output")
 
-    def process_run(run):
-        data = {"contact_uuid": run.contact.uuid, "run_id": run.id}
+    # Convert the RapidPro run objects to TracedData.
+    traced_runs = []
+    for run in runs:
+        run_dict = {"contact_uuid": run.contact.uuid}  # TODO: Replace with phone number uuid table.
 
         for category, response in run.values.items():
-            data[category.title() + " (Category) - " + run.flow.name] = response.category
-            data[category.title() + " (Value) - " + run.flow.name] = response.value
-            data[category.title() + " (Time) - " + run.flow.name] = response.time.isoformat()
+            run_dict[category.title() + " (Category) - " + run.flow.name] = response.category
+            run_dict[category.title() + " (Value) - " + run.flow.name] = response.value
+            # Convert from "input" to "text" here to match terminology in RP's Excel exports.
+            run_dict[category.title() + " (Text) - " + run.flow.name] = response.input
+            run_dict[category.title() + " (Name) - " + run.flow.name] = response.name
+            run_dict[category.title() + " (Time) - " + run.flow.name] = response.time.isoformat()
+            run_dict[category.title() + " (Run ID) - " + run.flow.name] = run.id
 
-        data["created_on"] = run.created_on.isoformat()
-        data["modified_on"] = run.modified_on.isoformat()
-        data["exited_on"] = run.exited_on.isoformat()
-        data["exit_type"] = run.exit_type
+        if mode == "messages":
+            run_dict["created_on"] = run.created_on.isoformat()
+            run_dict["modified_on"] = run.modified_on.isoformat()
+            run_dict["exited_on"] = run.exited_on.isoformat()
+            run_dict["exit_type"] = run.exit_type
 
-        return TracedData(data, Metadata(user, Metadata.get_call_location(), time.time()))
+        traced_runs.append(TracedData(run_dict, Metadata(user, Metadata.get_call_location(), time.time())))
 
-    # Convert runs to TracedData.
-    traced_runs = list(map(process_run, runs))
+    if mode == "survey":
+        # Keep only the latest values for each node for each contact
+        contacts = dict()  # of contact_uuid -> parsed_run
+        for run in traced_runs:
+            contact = run["contact_uuid"]
+            if contact not in contacts:
+                contacts[contact] = run
+            else:
+                contacts[contact].append_data(
+                    dict(filter(lambda x: x[0] != "contact_uuid", run.items())),
+                    Metadata(user, Metadata.get_call_location(), time.time())
+                )
+        traced_runs = contacts.values()
 
     # Output TracedData to JSON.
     if os.path.dirname(output_path) is not "" and not os.path.exists(os.path.dirname(output_path)):
