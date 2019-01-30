34c49b62b5cc888a29d970a2c770922d3d7094b3
Alexander Simpson <as2388@cam.ac.uk>
2019-01-30 16:20:30 +0000

    Add fetch_flow_definitions for fetching the flow + trigger definitions for the specified list of flow names

---
+++ fetch_flow_definitions/fetch_flow_definitions.py
@@ -0,0 +1,46 @@
+import argparse
+import json
+import time
+
+from temba_client.v2 import TembaClient
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Downloads flow definitions from Rapid Pro and writes to a json file")
+    parser.add_argument("server", help="Address of RapidPro server")
+    parser.add_argument("token", help="RapidPro API Token")
+    parser.add_argument("flow_names", help="Names of the flows to download the definitions of",
+                        nargs="+", default=None, metavar="flow-names")
+    parser.add_argument("output_file_path", metavar="output-file-path",
+                        help="Path to a JSON file to write the downloaded definitions to")
+
+    args = parser.parse_args()
+    server = args.server
+    flow_names = args.flow_names
+    token = args.token
+    output_file_path = args.output_file_path
+
+    rapid_pro = TembaClient(server, token)
+
+    flow_ids = []
+    for flow_name in flow_names:
+        print("Determining id for flow `{}`...".format(flow_name))
+        flows = rapid_pro.get_flows().all(retry_on_rate_exceed=True)
+        matching_flows = [f for f in flows if f.name == flow_name]
+
+        if len(matching_flows) == 0:
+            raise KeyError("Requested flow not found on RapidPro (Available flows: {})".format(
+                           ",".join(list(map(lambda f: f.name, flows)))))
+        if len(matching_flows) > 1:
+            raise KeyError("Non-unique flow name")
+
+        flow_id = matching_flows[0].uuid
+        flow_ids.append(flow_id)
+
+    # Download the flow definition(s) from Rapid Pro
+    print(f"Fetching definitions for {len(flow_ids)} flow(s)...")
+    start = time.time()
+    definitions = rapid_pro.get_definitions(flows=flow_ids, dependencies="none")
+    print(f"Fetched flow definitions ({time.time() - start}s)")
+
+    with open(output_file_path, "w") as f:
+        json.dump(definitions.serialize(), f, indent=2)
