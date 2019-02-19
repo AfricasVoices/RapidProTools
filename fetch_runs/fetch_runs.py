92bf368b29d09b20e86dfba8cac996a9408110fb
Alexander Simpson <as2388@cam.ac.uk>
2019-02-19 17:50:04 +0000

    Consistently use brackets when complaining about rapid pro contact uuids

--- fetch_runs/fetch_runs.py
+++ fetch_runs/fetch_runs.py
@@ -123,8 +123,8 @@ if __name__ == "__main__":
         contact_urns = contact_runs[run.contact.uuid].urns
         
         if len(contact_urns) == 0:
-            print(f"Warning: Ignoring contact with no urn. URNs: {contact_urns}, "
-                  f"Rapid Pro Contact UUID: {run.contact.uuid}")
+            print(f"Warning: Ignoring contact with no urn. URNs: {contact_urns} "
+                  f"(Rapid Pro Contact UUID: {run.contact.uuid})")
             continue
         
         run_dict = {
