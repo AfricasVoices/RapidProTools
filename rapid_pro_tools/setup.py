0305d3d7d70e5bfc5769aa6f36b23e2eec850b19
Alexander Simpson <as2388@cam.ac.uk>
2019-03-04 17:44:57 +0000

    Add #egg to dependency_link

--- rapid_pro_tools/setup.py
+++ rapid_pro_tools/setup.py
@@ -6,5 +6,5 @@ setup(
     url="https://github.com/AfricasVoices/RapidProTools",
     packages=["rapid_pro_tools"],
     install_requires=["core_data_modules"],
-    dependency_links=["git+https://git@github.com/AfricasVoices/CoreDataModules.git"]
+    dependency_links=["git+https://git@github.com/AfricasVoices/CoreDataModules.git#egg=core_data_modules"]
 )
