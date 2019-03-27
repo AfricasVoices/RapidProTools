c0d618acba2a86854102c63346527200a310f759
Alexander Simpson <as2388@cam.ac.uk>
2019-03-27 17:26:08 +0000

    Add missing comma to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.0",
     url="https://github.com/AfricasVoices/RapidProTools",
     packages=["rapid_pro_tools"],
-    install_requires=["CoreDataModules", "Pipeline-Infrastructure" "rapidpro-python", "python-dateutil"],
+    install_requires=["CoreDataModules", "Pipeline-Infrastructure", "rapidpro-python", "python-dateutil"],
     dependency_links=[
         "git+https://git@github.com/AfricasVoices/CoreDataModules.git#egg=CoreDataModules",
         "git+https://git@github.com/AfricasVoices/Pipeline-Infrastructure.git#egg=Pipeline-Infrastructure",
