c4edefa8040fd5d2bf03588f234e57aa53dc6a44
Alexander Simpson <as2388@cam.ac.uk>
2019-04-18 16:40:13 +0100

    Download messages from the specified date

--- message_stats.py
+++ message_stats.py
@@ -1,4 +1,5 @@
 import argparse
+import datetime
 import json
 
 from core_data_modules.cleaners import PhoneCleaner
@@ -23,45 +24,11 @@ rapid_pro = RapidProClient(server, token)
 
 # Get messages last updated today
 raw_messages = rapid_pro \
-    .get_raw_messages(last_modified_after_inclusive=date)\
+    .get_raw_messages(created_after_inclusive=date, created_before_exclusive=date + datetime.timedelta(days=1))\
     .all(retry_on_rate_exceed=True)
 log.info(f"Fetched {len(raw_messages)} raw messages last modified since {date.isoformat()}")
 
-# Filter for incoming messages only
-incoming_messages = [msg for msg in raw_messages if msg.direction == "in"]
-log.info(f"Filtered for incoming messages only: {len(incoming_messages)}/{len(raw_messages)} messages remain.")
-
-# Filter for messages actually received today
-incoming_messages_today = [msg for msg in incoming_messages if msg.sent_on > date]
-log.info(f"Filtered for messages actually sent since {date.isoformat()}: "
-         f"{len(incoming_messages_today)}/{len(incoming_messages)} messages remain.")
-
-# Filter for messages received from numbers
-incoming_phone_messages_today = [msg for msg in incoming_messages_today if msg.urn.startswith("tel:")]
-log.info(f"Filtered for messages sent from a telephone: "
-         f"{len(incoming_phone_messages_today)}/{len(incoming_messages_today)} messages remain.")
-
-# Compute number of messages received by each operator
-operator_counts = dict()  # of operator -> count
-for msg in incoming_phone_messages_today:
-    phone_number = msg.urn.replace("tel:", "")
-    operator = PhoneCleaner.clean_operator(phone_number)
-    if operator not in operator_counts:
-        operator_counts[operator] = 0
-    operator_counts[operator] += 1
-
-for operator, count in operator_counts.items():
-    print(f"{operator}: {count}")
-
-
-# for msg in raw_messages:
-#     if msg.direction == "in":
-#         print(json.dumps(msg.serialize(), indent=2))
-
-# sample_message = messages_today[0]
-# print(json.dumps(sample_message.serialize(), indent=2))
-
-
+log.info("Counting the number of messages in/out/failed per operator...")
 operator_counts = dict()  # of operator -> category -> count
 for msg in raw_messages:
     phone_number = msg.urn.replace("tel:", "")
