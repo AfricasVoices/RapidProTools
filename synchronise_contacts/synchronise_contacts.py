8c6e866676ab38c4842785afedd723965dd5062b
Alexander Simpson <as2388@cam.ac.uk>
2019-10-17 12:35:08 +0100

    Change merge strategy to update with latest if -f flag passed, otherwise do nothing

--- synchronise_contacts/synchronise_contacts.py
+++ synchronise_contacts/synchronise_contacts.py
@@ -11,6 +11,8 @@ log.set_project_name("SynchroniseContacts")
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Migrates contacts from one Rapid Pro instance to another")
 
+    parser.add_argument("-f", "--force", const=True, default=False, action="store_const",
+                        help="Overwrite contacts which differ in each instance with the latest")
     parser.add_argument("google_cloud_credentials_file_path", metavar="google-cloud-credentials-file-path",
                         help="Path to a Google Cloud service account credentials file to use to access the "
                              "credentials bucket")
@@ -26,6 +28,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     google_cloud_credentials_file_path = args.google_cloud_credentials_file_path
+    force_update = args.force
+
     instance_1_domain = args.instance_1_domain
     instance_1_credentials_url = args.instance_1_credentials_url
     instance_2_domain = args.instance_2_domain
@@ -58,76 +62,70 @@ if __name__ == "__main__":
     log.info("Synchronising contacts...")
     instance_1_contacts = instance_1.get_raw_contacts()
     instance_2_contacts = instance_2.get_raw_contacts()
+    
+    def filter_valid_contacts(contacts):
+        valid_contacts = []
+        for contact in contacts:
+            if len(contact.urns) != 1:
+                log.warning(f"Found a contact in the source instance with multiple URNS; skipping. "
+                            f"The RapidPro UUID is '{contact.uuid}'")
+                continue
+            if contact.urns[0].startswith("tel:") and not contact.urns[0].startswith("tel:+"):
+                log.warning(f"Found a contact with a telephone number but without a country code; skipping. "
+                            f"The RapidPro UUID is '{contact.uuid}'")
+                continue
+            valid_contacts.append(contact)
+        return valid_contacts
 
-    for contact in instance_1_contacts:
-        if len(contact.urns != 1):
-            log.warning(f"Found a contact in the source instance with multiple URNS. "
-                        f"The RapidPro UUID is '{contact.uuid}' in instance 1")
-    for contact in instance_2_contacts:
-        if len(contact.urns != 1):
-            log.warning(f"Found a contact in the source instance with multiple URNS. "
-                        f"The RapidPro UUID is '{contact.uuid}' in instance 2")
+    log.info("Filtering out invalid contacts from instance 1...")
+    instance_1_contacts = filter_valid_contacts(instance_1_contacts)
+    log.info("Filtering out invalid contacts from instance 2...")
+    instance_2_contacts = filter_valid_contacts(instance_2_contacts)
 
+    # Trim URN metadata because although Rapid Pro sometimes provides some in its get APIs, it refuses them when setting
     instance_1_contacts_lut = {c.urns[0].split("#")[0]: c for c in instance_1_contacts}
     instance_2_contacts_lut = {c.urns[0].split("#")[0]: c for c in instance_2_contacts}
 
-    # Update contacts in instance 1 but not in instance 2
+    # Update contacts present in instance 1 but not in instance 2
     urns_unique_to_instance_1 = instance_1_contacts_lut.keys() - instance_2_contacts_lut.keys()
     for i, urn in enumerate(urns_unique_to_instance_1):
         log.info(f"Adding new contacts to instance 2: {i + 1}/{len(urns_unique_to_instance_1)}")
         contact = instance_1_contacts_lut[urn]
         instance_2.update_contact(contact.urns[0], contact.name, contact.fields)
 
-    # Update contacts in instance 2 but not in instance 1
+    # Update contacts present in instance 2 but not in instance 1
     urns_unique_to_instance_2 = instance_2_contacts_lut.keys() - instance_1_contacts_lut.keys()
     for i, urn in enumerate(urns_unique_to_instance_2):
         log.info(f"Adding new contacts to instance 1: {i + 1}/{len(urns_unique_to_instance_2)}")
         contact = instance_2_contacts_lut[urn]
         instance_1.update_contact(contact.urns[0], contact.name, contact.fields)
 
-    # Update contacts in both instances
+    # Update contacts present in both instances
     urns_in_both_instances = instance_1_contacts_lut.keys() & instance_2_contacts_lut.keys()
     for i, urn in enumerate(urns_in_both_instances):
         contact_v1 = instance_1_contacts_lut[urn]
         contact_v2 = instance_2_contacts_lut[urn]
 
         if contact_v1.name == contact_v2.name and contact_v1.fields == contact_v2.fields:
-            log.info(f"Synchronising contacts in both instances: {i + 1}/{len(urns_in_both_instances)} (contacts identical)")
+            log.info(f"Synchronising contacts in both instances {i + 1}/{len(urns_in_both_instances)}: "
+                     f"Contacts identical")
             continue
 
-        # Contacts differ. Assume the most recent contact is correct.
+        # Contacts differ
+        if not force_update:
+            log.warning(f"Synchronising contacts in both instances {i + 1}/{len(urns_in_both_instances)}: "
+                        f"Contacts differ, but not overwriting. Use --force to write the latest everywhere. "
+                        f"Rapid Pro UUIDs are '{contact_v1.uuid}' in instance 1; '{contact_v2.uuid}' in instance 2")
+            continue
+            
+        # Assume the most recent contact is correct
         # IMPORTANT: If the same contact has been changed on both Rapid Pro instances since the last sync was performed,
         #            the older changes will be overwritten.
-        #            If contacts differ in each instance, interim updates on Rapid Pro may be overwritten.
-        name = None
-        fields = None
         if contact_v1.modified_on > contact_v2.modified_on:
-            name = contact_v1.name
-            fields = contact_v2.fields
-            fields.update(contact_v1.fields)
+            log.info(f"Synchronising contacts in both instances {i + 1}/{len(urns_in_both_instances)}: "
+                     f"Contacts differ, overwriting the contact in instance 2 with the more recent one in instance 1")
+            instance_2.update_contact(urn, contact_v1.name, contact_v1.fields)
         else:
-            name = contact_v2.name
-            fields = contact_v1.fields
-            fields.update(contact_v2.fields)
-
-        # Drop unnecessary field updates.
-        # This reduces traffic, and the risk of overwriting fields which have been updated in Rapid Pro since the
-        # contacts were fetched (large syncs can take hours to days)
-        for f in list(fields):
-            if contact_v1.fields[f] == contact_v2.fields[f]:
-                del fields[f]
-            if fields[f] is None:
-                del fields[f]
-
-        if urn.startswith("tel:") and not urn.startswith("tel:+"):
-            log.warning(f"Found a contact in the source instance without a telephone number without a country "
-                        f"code; skipping. The RapidPro UUID is '{contact_v1.uuid}' in instance 1;"
-                        f"'{contact_v2.uuid}' in instance 2")
-            continue
-
-        if name == "":
-            name = None
-
-        log.info(f"Synchronising contacts in both instances: {i + 1}/{len(urns_in_both_instances)} (contacts differ)")
-        instance_1.update_contact(urn, name, fields)
-        instance_2.update_contact(urn, name, fields)
+            log.info(f"Synchronising contacts in both instances {i + 1}/{len(urns_in_both_instances)}: "
+                     f"Contacts differ, overwriting the contact in instance 1 with the more recent one in instance 2")
+            instance_1.update_contact(urn, contact_v2.name, contact_v2.fields)
