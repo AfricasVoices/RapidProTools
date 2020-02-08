0f2277401724f3337efe785576535b476f456466
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-02-08 19:14:21 +0300

    updated script to use get messages func from rapidpro

--- mno_analysis_tools/fetch_archives.py
+++ mno_analysis_tools/fetch_archives.py
@@ -1,48 +1,31 @@
-import json
 import argparse
 
 from core_data_modules.logging import Logger
 from rapid_pro_tools import RapidProClient
-from temba_client.v2 import Message
 
 log = Logger(__name__)
-log.set_project_name("FetchArchives")
+log.set_project_name("FetchRawMessages")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Fetches archives from Rapid Pro instance"
-    )
-
-    parser.add_argument(
-        "domain",
-        metavar="domain",
+    parser = argparse.ArgumentParser(description="Fetches archives from Rapid Pro instance")
+    parser.add_argument("domain", metavar="domain",
         help="Domain that the instance of Rapid Pro is running on",
     )
-    parser.add_argument(
-        "token",
-        metavar="token",
-        help="token for authenticating to the instance",
+    parser.add_argument("token", metavar="token",
+        help="Token for authenticating to the instance",
+    )
+    parser.add_argument("output_file", metavar="file", type=argparse.FileType(mode="w"),
+        help="File to write the raw data downloaded as json.",
     )
 
     args = parser.parse_args()
-
+  
     source_domain = args.domain
     source_token = args.token
+    output_file = args.output_file
+
     source_instance = RapidProClient(source_domain, source_token)
 
-    available_archives_list = source_instance.list_archives("message")
-    log.info(f"Fetched {len(available_archives_list)} archives")
-
-    list_of_message_objects = []
-    for archive_metadata in available_archives_list:
-        list_of_message_objects.append(source_instance.get_archive(archive_metadata))
-
-    output = [
-        Message.serialize(attribute)
-        for message_object in list_of_message_objects
-        for attribute in message_object
-    ]
-
-    with open("data.txt", mode="w") as f:
-        f.write(json.dumps(output))
+    log.info("fetch raw messages...")
+    source_instance.get_raw_messages(raw_export_log_file=output_file)
