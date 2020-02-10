417357a2a2b490234c3ba0aa32d47db960bc1deb
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-02-10 23:25:34 +0300

    renamed script

---
+++ mno_analysis_tools/fetch_raw_messages.py
@@ -0,0 +1,31 @@
+import argparse
+
+from core_data_modules.logging import Logger
+from rapid_pro_tools import RapidProClient
+
+log = Logger(__name__)
+log.set_project_name("FetchRawMessages")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Fetches archives from Rapid Pro instance")
+    parser.add_argument("domain", metavar="domain",
+        help="Domain that the instance of Rapid Pro is running on",
+    )
+    parser.add_argument("token", metavar="token",
+        help="Token for authenticating to the instance",
+    )
+    parser.add_argument("output_file", metavar="file", type=argparse.FileType(mode="w"),
+        help="Path to the JSON file to write the downloaded messages to",
+    )
+
+    args = parser.parse_args()
+  
+    source_domain = args.domain
+    source_token = args.token
+    output_file = args.output_file
+
+    source_instance = RapidProClient(source_domain, source_token)
+
+    log.info("fetch raw messages...")
+    source_instance.get_raw_messages(raw_export_log_file=output_file)
