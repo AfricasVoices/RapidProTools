4de2a7c2547f0b81c864e416abf2313478246d66
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-02-12 12:21:21 +0300

    updated script to compute window of downtime

--- mno_analysis_tools/compute_window_of_downtime.py
+++ mno_analysis_tools/compute_window_of_downtime.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 from temba_client.v2 import Message
 from core_data_modules.logging import Logger
+from dateutil.parser import isoparse
 
 
 log = Logger(__name__)
@@ -12,10 +13,10 @@ log.set_project_name("ComputeWindowOfDowntime")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Compute maximum window of time with 0 messages")
-    parser.add_argument("input_file", metavar="input file", type=argparse.FileType(mode="r"),
+    parser.add_argument("input_file", metavar="input file", 
         help="File to read the raw data downloaded as json.",
     )
-    parser.add_argument("output_file", metavar="output file", type=argparse.FileType(mode="w"),
+    parser.add_argument("output_file", metavar="output file",
         help="File to write the raw data downloaded as json.",
     )
     parser.add_argument("communication_medium", metavar="operator",
@@ -24,16 +25,15 @@ if __name__ == "__main__":
     parser.add_argument("direction", metavar="direction of message",
         help="Direction of the message either sent or received by the operator",
     )
-    parser.add_argument("start_date", metavar="start date", 
-        type=lambda s: datetime.strptime(s, '%Y-%m-%d').astimezone(pytz.UTC),
-        help="The start date (yyyy-mm-dd) from which the window of downtime will be computed.",
+    parser.add_argument("start_date", metavar="start date", type=lambda s: isoparse(s),
+        help="The start date as ISO 8601 string from which the window of downtime will be computed.",
     )
-    parser.add_argument("end_date", metavar="end date",
-        type=lambda s: datetime.strptime(s, '%Y-%m-%d').astimezone(pytz.UTC),
-        help="The end date (yyy-mm-dd) to which the window of downtime computation will end",
+    parser.add_argument("end_date", metavar="end date", type=lambda s: isoparse(s),
+        help="The end date as ISO 8601 string to which the window of downtime computation will end",
     )
 
     args = parser.parse_args()
+    print(type(args.start_date))
 
     input_file = args.input_file
     output_file = args.output_file
@@ -42,11 +42,11 @@ if __name__ == "__main__":
     start_date = args.start_date
     end_date = args.end_date
 
-    with input_file as file:
-        file_content = file.readline()
-        output = json.loads(file_content)
+    with open(input_file) as f:
+        output = json.loads(f.read())
         messages = [Message.deserialize(val) for val in output]
-        log.info(f"Read {len(messages)} messages...")
+        log.info("Loading messages from file {file_name}...")
+        log.info(f"Loaded {len(messages)} messages")
 
     period_with_msg = []
     generated_outputs = []
@@ -57,7 +57,7 @@ if __name__ == "__main__":
         direction = msg.direction
         if communication_medium == operator and direction == msg_direction:
             period_with_msg.append(msg.sent_on)
-    period_with_msg.insert(len(period_with_msg), end_date)
+    period_with_msg.append(end_date)
     
     for index, time_in_range in enumerate(period_with_msg):
         log.debug(
@@ -72,8 +72,8 @@ if __name__ == "__main__":
 
         time_diff = period_with_msg[next_index] - period_with_msg[index]
         generated_outputs.append({
-            "communication_medium" : communication_medium,
-            "direction" : direction,
+            "communication_medium" : operator,
+            "direction" : msg_direction,
             "start" : str(period_with_msg[index]),
             "end" : str(period_with_msg[next_index]),
             "delta" : str(abs(time_diff.total_seconds()))
@@ -81,8 +81,8 @@ if __name__ == "__main__":
              
     if output_file is not None:
         log.info(f"Logging {len(generated_outputs)} generated messages...")
-        json.dump(generated_outputs, output_file)
-        output_file.write("\n")
+        with open(output_file, mode="w") as f:
+            json.dump(generated_outputs, f)
         log.info(f"Logged generated messages")
     else:
         log.debug("Not logging, the output file was not specified")
