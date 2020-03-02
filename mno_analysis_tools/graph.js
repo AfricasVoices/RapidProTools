e043d21ea93dc718aa0a32ae31d1d2806af061bd
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-03-02 15:41:32 +0300

    Set the dimensions and margins of the graphs

---
+++ mno_analysis_tools/graph.js
@@ -0,0 +1,4 @@
+// Set the dimensions and margins of the graphs
+const margin = { top: 20, right: 30, bottom: 70, left: 50 },
+    graphWidth = 960 - margin.left - margin.right,
+    graphHeight = 500 - margin.top - margin.bottom;
