610f616dfff09c04f9ea04f8644c008e2a73c5c9
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-03-02 15:45:37 +0300

    define outgoing messages graph layout, scale, axis groups, and line plot

--- mno_analysis_tools/graph.js
+++ mno_analysis_tools/graph.js
@@ -42,3 +42,43 @@ const incomingMsgGraphSvg = d3
     // d3 line path generator
     incomingMsgGraphNumberOfMessagesLinePath = incomingMsgGraph.append("path"),
     incomingMsgGraphMessageDifferenceLinePath = incomingMsgGraph.append("path");
+
+// Outgoing Messages
+// Svg
+const outgoingMsgGraphSvg = d3
+        .select(".outgoing-graph")
+        .append("svg")
+        .attr("width", graphWidth + margin.left + margin.right)
+        .attr("height", graphHeight + margin.top + margin.bottom),
+    // Graph
+    outgoingMsgGraph = outgoingMsgGraphSvg
+        .append("g")
+        .attr("width", graphWidth)
+        .attr("Height", graphHeight)
+        .attr("transform", `translate(${margin.left}, ${margin.top})`),
+    // Scales
+    outgoingMsgGraphxScale = d3.scaleTime().range([0, graphWidth]),
+    outgoingMsgGraphLeftyScale = d3.scaleLinear().range([graphHeight, 0]),
+    outgoingMsgGraphRightyScale = d3.scaleLinear().range([graphHeight, 0]),
+    // Axes groups
+    outgoingMsgGraphxAxisGroup = outgoingMsgGraph
+        .append("g")
+        .attr("class", "x-axis")
+        .attr("transform", "translate(0," + graphHeight + ")"),
+    outgoingMsgGraphLeftYAxisGroup = outgoingMsgGraph.append("g").attr("class", "left-y-axis"),
+    outgoingMsgGraphRightYAxisGroup = outgoingMsgGraph
+        .append("g")
+        .attr("transform", "translate( " + graphWidth + ", 0 )")
+        .attr("class", "right-y-axis"),
+    // Line
+    outgoingMsgGraphMessageDifferenceLine = d3
+        .line()
+        .x(d => outgoingMsgGraphxScale(new Date(d.PeriodBetween)))
+        .y(d => outgoingMsgGraphRightyScale(d.MessageDifference)),
+    outgoingMsgGraphNumberOfMessagesLine = d3
+        .line()
+        .x(d => outgoingMsgGraphxScale(new Date(d.PeriodEnd)))
+        .y(d => outgoingMsgGraphRightyScale(d.NumberOfMessages)),
+    // d3 line path generator
+    outgoingMsgGraphNumberOfMessagesLinePath = outgoingMsgGraph.append("path"),
+    outgoingMsgGraphMessageDifferenceLinePath = outgoingMsgGraph.append("path");
