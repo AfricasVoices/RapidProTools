8fefb1de78a26ecea70c470203edaa472e3359e1
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-03-05 16:58:22 +0300

    remove action argument from parser

--- mno_analysis_tools/compute_msg_difference_btwn_periods.py
+++ mno_analysis_tools/compute_msg_difference_btwn_periods.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
                         help="The start date as ISO 8601 string from which the number of messages will be computed")
     parser.add_argument("end_date", metavar="end-date", type=lambda s: isoparse(s),
                         help="The end date as ISO 8601 string to which the number of messages computation will end")
-    parser.add_argument("-t", "--time_frame", action="store_true", metavar="time-frame", type=lambda s: datetime.strptime(s, '%H:%M:%S'),
+    parser.add_argument("-t", "--time_frame", metavar="time-frame", type=lambda s: datetime.strptime(s, '%H:%M:%S'),
                         default="00:00:10", help="The time frame (HH:MM:SS) to generate dates in intervals between the start and end date")
 
     args = parser.parse_args()
