791e0bb0aa991ca79070d7b644441f5996397425
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-03-05 17:42:06 +0300

    Swap words on parser help message for clarity
    Co-Authored-By: Isaack Mwenda <mwendaisaac@gmail.com>

--- mno_analysis_tools/compute_window_of_downtime.py
+++ mno_analysis_tools/compute_window_of_downtime.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Compute maximum window of time with 0 messages")
     parser.add_argument("raw_messages_file_path", metavar="input-file",
-                        help="File to read the raw data downloaded as json")
+                        help="File to read the raw messages data downloaded as JSON")
     parser.add_argument("window_of_downtimes_output_file_path", metavar="output-file",
                         help="File to write the raw data downloaded as json.")
     parser.add_argument("target_operator", metavar="operator",
