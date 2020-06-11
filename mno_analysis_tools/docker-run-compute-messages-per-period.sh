7a55aa9e2d9012511b62a76212c07e4ff319cfbd
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-06-11 11:37:37 +0300

    script to compute messages per period

---
+++ mno_analysis_tools/docker-run-compute-messages-per-period.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -e
+
+IMAGE_NAME=compute-messages-per-period
+
+# Check that the correct number of arguments were provided.
+if [[ $# -ne 7 ]]; then
+    echo "Usage: ./docker-run-compute-window-of-downtime.sh 
+    <raw_messages_file_path> <target_operator> <target_message_direction> 
+    <start_date> <end_date> <time_frame> <output_dir>" 
+    exit   
+fi
+ 
+# Assign the program arguments to bash variables.
+RAW_MESSAGES_FILE_PATH=$1
+TARGET_OPERATOR=$2
+TARGET_MESSAGE_DIRECTION=$3
+START_DATE=$4
+END_DATE=$5
+TIME_FRAME=$6
+OUTPUT_DIR=$7
+
+# Build an image for this pipeline stage.
+docker build -t "$IMAGE_NAME" .
+
+if [[ $TARGET_MESSAGE_DIRECTION == "in" ]] 
+then
+   MSG_DIRECTION="incoming"
+else
+   MSG_DIRECTION="outgoing"
+fi
+
+CMD="pipenv run python -u compute_messages_per_period.py \
+    /data/raw_messages.json /data/${MSG_DIRECTION}_msg.json \
+    \"$TARGET_OPERATOR\" \"$TARGET_MESSAGE_DIRECTION\"  \"$START_DATE\" \"$END_DATE\" \"$TIME_FRAME\"
+"
+
+container="$(docker container create -w /app "$IMAGE_NAME" /bin/bash -c "$CMD")"
+echo "Created container $container"
+container_short_id=${container:0:7}
+
+function finish {
+    # Tear down the container when done.
+    docker container rm "$container" >/dev/null
+}
+trap finish EXIT
+
+# Copy input data into the container
+docker cp "$RAW_MESSAGES_FILE_PATH" "$container:/data/raw_messages.json"
+
+# Run the container
+docker start -a -i "$container"
+
+# Copy the output data back out of the container
+echo "Copying $container_short_id:/data/. -> $OUTPUT_DIR"
+# mkdir -p "$OUTPUT_DIR"
+docker cp "$container:/data/." "$OUTPUT_DIR"
