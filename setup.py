342f59380760e37770c22e9f28a5768758ee3397
Alexander Simpson <as2388@cam.ac.uk>
2020-09-28 10:41:43 +0100

    Version bump for release v0.3.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="RapidProTools",
-    version="0.3.5",
+    version="0.3.6",
     url="https://github.com/AfricasVoices/RapidProTools",
     packages=["rapid_pro_tools"],
     install_requires=["rapidpro-python", "python-dateutil",
