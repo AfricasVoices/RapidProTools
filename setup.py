7d5e6f35ab721c719778059899851be49704788f
Alexander Simpson <as2388@cam.ac.uk>
2023-07-31 14:30:57 +0100

    Remove git subdep

--- setup.py
+++ setup.py
@@ -5,6 +5,5 @@ setup(
     version="0.3.9",
     url="https://github.com/AfricasVoices/RapidProTools",
     packages=["rapid_pro_tools"],
-    install_requires=["rapidpro-python==2.12.0", "python-dateutil>=2,<3",
-                      "coredatamodules @ git+https://github.com/AfricasVoices/CoreDataModules"]
+    install_requires=["rapidpro-python==2.12.0", "python-dateutil>=2,<3"]
 )
