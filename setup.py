4363e89ed7e97dcf123b8ff35909aaef4b264823
Alexander Simpson <as2388@cam.ac.uk>
2023-07-31 11:46:55 +0100

    Explicitly set subdependncy versions

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version="0.3.9",
     url="https://github.com/AfricasVoices/RapidProTools",
     packages=["rapid_pro_tools"],
-    install_requires=["rapidpro-python", "python-dateutil",
+    install_requires=["rapidpro-python~2", "python-dateutil~2",
                       "coredatamodules @ git+https://github.com/AfricasVoices/CoreDataModules"]
 )
