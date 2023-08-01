3ceff62d4c269c3b2829f51a9fb87704b5996116
Alexander Simpson <as2388@cam.ac.uk>
2023-08-01 11:58:29 +0100

    Switch to pyproject.toml

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="RapidProTools",
-    version="0.3.9",
-    url="https://github.com/AfricasVoices/RapidProTools",
-    packages=["rapid_pro_tools"],
-    install_requires=["rapidpro-python==2.12.0", "python-dateutil>=2,<3"]
-)
